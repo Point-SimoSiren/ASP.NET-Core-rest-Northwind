1c41926520f7680c06d18b8779fb05949049e2e3
Simo Siren <simo.siren@stud.pointcollege.fi>
2019-11-01 11:03:11 +0200

    Tilanne ennen tuote, tilaus ja asiakastietojen sisällyttämistä api palveluun

--- CoreApiHarj/Controllers/CustomerController.cs
+++ CoreApiHarj/Controllers/CustomerController.cs
@@ -6,6 +6,8 @@ using CoreApiHarj.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+//Tämä kontrolleri on päivitetty kurssin koodimallista, selventävien kommenttejen vuoksi.
+
 namespace CoreApiHarj.Controllers
 {
     [Route("nw/[controller]")]
@@ -14,7 +16,7 @@ namespace CoreApiHarj.Controllers
     {
         [HttpGet]
         [Route("")]
-        public List<Customers> GetAllCustomers() // KAIKKI RIVIT
+        public List<Customers> GetAllCustomers() //Hakee kaikki rivit
         {
             northwindContext db = new northwindContext();
             List<Customers> asiakkaat = db.Customers.ToList();
@@ -23,13 +25,78 @@ namespace CoreApiHarj.Controllers
 
         [HttpGet]
         [Route("{id}")]
-        public Customers GetCustomerById(string id) // FIND hakee pääavaimella yhden rivin
+        public Customers GetOneCustomer(string id) //Find-metodi hakee AINA VAIN PÄÄAVAIMELLA YHDEN RIVIN
         {
             northwindContext db = new northwindContext();
+            Customers asiakas = db.Customers.Find(id);
+            return asiakas;
+        }
+
+        [HttpGet]
+        [Route("country/{key}")]
+        public List<Customers> GetSomeCustomers(string key) //Hakee jollain tiedolla mätsäävät rivit
+        {
+            northwindContext db = new northwindContext();
+
+            var someCustomers = from c in db.Customers
+                                where c.Country == key
+                                select c;
+
+            return someCustomers.ToList();
+        }
+
+        [HttpPost] //<-- filtteri, joka sallii vain POST-metodit (Http-verbit)
+        [Route("")] //<-- Routen placeholder
+        public ActionResult PostCreateNew([FromBody] Customers asiakas) //<-- [FromBody] tarkoittaa, että HTTP-pyynnön Body:ssä välitetään JSON-muodossa oleva objekti ,joka on Customers-tyyppinen asiakas-niminen
+        {
+            northwindContext db = new northwindContext(); //Context = Kuten entities muodostettu Scaffold DBContext -työkalulla. Voisi olla myös entiteetti frameworkCore
             try
             {
-                Customers Asiakas = db.Customers.Find(id);
-                return Asiakas;
+                db.Customers.Add(asiakas);
+                db.SaveChanges();
+                return Ok(asiakas.CustomerId); //kuittaus Frontille, että päivitys meni oikein --> Frontti voi tsekata, että kontrolleri palauttaa järkevän arvon ja OK:n
+            }
+            catch (Exception)
+            {
+                return BadRequest("Jokin meni pieleen asiakasta lisättäessä, ota yhteyttä kuruun");
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+        }
+
+        [HttpPut] //<-- filtteri, joka sallii vain PUT-metodit (Http-verbit)
+        [Route("{key}")] //<-- Routemääritys asiakasavaimelle key=CustomerID
+        public ActionResult PutEdit(string key, [FromBody] Customers asiakas) //<-- [FromBody] tarkoittaa, että HTTP-pyynnön Body:ssä välitetään JSON-muodossa oleva objekti ,joka on Customers-tyyppinen asiakas-niminen
+        {
+            northwindContext db = new northwindContext(); //Context = Kuten entities muodostettu Scaffold DBContext -työkalulla. Voisi olla myös entiteetti frameworkCore
+            try
+            {
+                Customers customer = db.Customers.Find(key);
+                if (customer != null)
+                {
+                    customer.CompanyName = asiakas.CompanyName;
+                    customer.ContactName = asiakas.ContactName;
+                    customer.ContactTitle = asiakas.ContactTitle;
+                    customer.Country = asiakas.Country;
+                    customer.Address = asiakas.Address;
+                    customer.City = asiakas.City;
+                    customer.PostalCode = asiakas.PostalCode;
+                    customer.Phone = asiakas.Phone;
+
+                    db.SaveChanges();
+                    return Ok(customer.CustomerId);
+                }
+                else
+                {
+                    return NotFound("Päivitettävää asiakasta ei löytynyt!");
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest("Jokin meni pieleen asiakasta päivitettäessä, ota yhteyttä kuruun");
             }
             finally
             {
@@ -37,23 +104,23 @@ namespace CoreApiHarj.Controllers
             }
         }
 
-        [HttpGet]
-        [Route("{country}/{key}")]
-        public List<Customers> GetCustomerByCountry(string key) // LINQ kysely key itse nimetty. Polkuun nw/customer/country/<haluttu maa>
+        [HttpDelete]
+        [Route("{key}")]
+        public ActionResult DeleteCustomer(string key)
         {
             northwindContext db = new northwindContext();
-            try
+            Customers asiakas = db.Customers.Find(key);
+            if (asiakas != null)
             {
-                var seulottu = from a in db.Customers
-                               where a.Country == key
-                               select a;
+                db.Customers.Remove(asiakas);
+                db.SaveChanges();
+                return Ok("Asiakas " + key + " poistettiin");
+            }
+            else
+            {
+                return NotFound("Asiakasta " + key + " ei löydy");
+            }
 
-                return seulottu.ToList();
-            }
-            finally
-            {
-                db.Dispose();
-            }
         }
     }
 }
---
+++ CoreApiHarj/Models/PublicDocument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApiHarj.Models
+{
+    public class PublicDocument
+    {
+        public int DocumentationId { get; set; }
+        public string AvailableRoute { get; set; }
+        public string Method { get; set; }
+        public string Description { get; set; }
+
+    }
+}
+
